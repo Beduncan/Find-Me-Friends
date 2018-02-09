328e4ae4345b7a03ebf2764156633d0232223818
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2018-02-09 09:02:02 -0700

    trying to fid array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,61 +1,20 @@
 var friendsArray = [
 {
-	"name":"The Boogie Man",
-	"image":"https://theliberatedeater.com/food-is-not-the-boogie-man/",
-	"q1":"5",
-	"q2":"4",
-	"q3":"4",
-	"q4":"4",
-	"q5":"4",
-	"q6":"3",
-	"q7":"3",
-	"q8":"4",
-	"q9":"3",
-	"q10":"3",
-	"q11":"4",
+	name:"The Boogie Man",
+	image:"https://theliberatedeater.com/food-is-not-the-boogie-man/",
+	scores: [5, 4, 4, 2 , 4, 3, 3 , 1]
 },{    
-	"name":"Batman",
-	"image":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fumad.com%2Fimg%2F2015%2F9%2Fbatman-cartoon-meme-6535-6852-hd-wallpapers.jpg&imgrefurl=https%3A%2F%2Fumad.com%2Fbatman-cartoon-meme-6535.html&docid=B59aaFAJxQ3J8M&tbnid=vZCoa-iLucdFtM%3A&vet=10ahUKEwiMreryqpDZAhUGw2MKHbdmAosQMwhDKA0wDQ..i&w=736&h=738&bih=668&biw=1396&q=batman%20meme&ved=0ahUKEwiMreryqpDZAhUGw2MKHbdmAosQMwhDKA0wDQ&iact=mrc&uact=8",
-	"q1":"2",
-	"q2":"4",
-	"q3":"3",
-	"q4":"5",
-	"q5":"2",
-	"q6":"5",
-	"q7":"2",
-	"q8":"1",
-	"q9":"1",
-	"q10":"5",
-	"q11":"1",
-    
+	name:"Batman",
+	image:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fumad.com%2Fimg%2F2015%2F9%2Fbatman-cartoon-meme-6535-6852-hd-wallpapers.jpg&imgrefurl=https%3A%2F%2Fumad.com%2Fbatman-cartoon-meme-6535.html&docid=B59aaFAJxQ3J8M&tbnid=vZCoa-iLucdFtM%3A&vet=10ahUKEwiMreryqpDZAhUGw2MKHbdmAosQMwhDKA0wDQ..i&w=736&h=738&bih=668&biw=1396&q=batman%20meme&ved=0ahUKEwiMreryqpDZAhUGw2MKHbdmAosQMwhDKA0wDQ&iact=mrc&uact=8",
+	scores: [2, 4, 3, 2, 2, 5, 5, 1, 1, 4, 1]
 },{    
-	"name":"Bill Murray",
-	"image":"https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.wweek.com%2Fresizer%2FFFITw38hXdAXTWs_FecdVu0Bog8%3D%2F1200x0%2Ffilters%3Aquality(100)%2Fs3.amazonaws.com%2Farc-wordpress-client-uploads%2Fwweek%2Fwp-content%2Fuploads%2F2017%2F11%2F28134502%2FBill-Murray.jpg&imgrefurl=http%3A%2F%2Fwww.wweek.com%2Farts%2Fmovies%2F2017%2F11%2F28%2Fbill-murray-is-hanging-out-in-downtown-portland-right-now%2F&docid=WszlmrKH0-vfOM&tbnid=wTVvbHfWmzhDtM%3A&vet=10ahUKEwjJkYKhq5DZAhVM-GMKHbTEDAUQMwg2KAQwBA..i&w=1200&h=675&bih=668&biw=1396&q=bill%20murray&ved=0ahUKEwjJkYKhq5DZAhVM-GMKHbTEDAUQMwg2KAQwBA&iact=mrc&uact=8",
-  	"q1":"3",
-	"q2":"5",
-	"q3":"1",
-	"q4":"4",
-	"q5":"2",
-	"q6":"5",
-	"q7":"1",
-	"q8":"2",
-	"q9":"5",
-	"q10":"1",
-	"q11":"5",
+	name:"Bill Murray",
+	image:"https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.wweek.com%2Fresizer%2FFFITw38hXdAXTWs_FecdVu0Bog8%3D%2F1200x0%2Ffilters%3Aquality(100)%2Fs3.amazonaws.com%2Farc-wordpress-client-uploads%2Fwweek%2Fwp-content%2Fuploads%2F2017%2F11%2F28134502%2FBill-Murray.jpg&imgrefurl=http%3A%2F%2Fwww.wweek.com%2Farts%2Fmovies%2F2017%2F11%2F28%2Fbill-murray-is-hanging-out-in-downtown-portland-right-now%2F&docid=WszlmrKH0-vfOM&tbnid=wTVvbHfWmzhDtM%3A&vet=10ahUKEwjJkYKhq5DZAhVM-GMKHbTEDAUQMwg2KAQwBA..i&w=1200&h=675&bih=668&biw=1396&q=bill%20murray&ved=0ahUKEwjJkYKhq5DZAhVM-GMKHbTEDAUQMwg2KAQwBA&iact=mrc&uact=8",
+  	scores: [ 3, 5, 1, 5, 2, 2, 2, 2, 5, 1, 5]
 },{    
-	"name":"MickeyMouse",
-	"image":"https://www.google.com/imgres?imgurl=http%3A%2F%2Fi0.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F000%2F028%2F513%2FMickey_Mouse.png&imgrefurl=http%3A%2F%2Fknowyourmeme.com%2Fphotos%2F28513-fsjal&docid=EANi0sl-YwjZpM&tbnid=l-v62Br_9tSAGM%3A&vet=10ahUKEwibt6L4q5DZAhUBRGMKHR3nDj0QMwhAKAcwBw..i&w=800&h=600&bih=668&biw=1396&q=mickey%20mouse%20meme&ved=0ahUKEwibt6L4q5DZAhUBRGMKHR3nDj0QMwhAKAcwBw&iact=mrc&uact=8",
-	"q1":"4",
-	"q2":"1",
-	"q3":"2",
-	"q4":"4",
-	"q5":"3",
-	"q6":"4",
-	"q7":"2",
-	"q8":"3",
-	"q9":"4",
-	"q10":"1",
-	"q11":"1",
+	name:"MickeyMouse",
+	image:"https://www.google.com/imgres?imgurl=http%3A%2F%2Fi0.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F000%2F028%2F513%2FMickey_Mouse.png&imgrefurl=http%3A%2F%2Fknowyourmeme.com%2Fphotos%2F28513-fsjal&docid=EANi0sl-YwjZpM&tbnid=l-v62Br_9tSAGM%3A&vet=10ahUKEwibt6L4q5DZAhUBRGMKHR3nDj0QMwhAKAcwBw..i&w=800&h=600&bih=668&biw=1396&q=mickey%20mouse%20meme&ved=0ahUKEwibt6L4q5DZAhUBRGMKHR3nDj0QMwhAKAcwBw&iact=mrc&uact=8",
+	scores: [4, 1, 2, 6, 3, 5, 3, 3, 4, 1, 1]
 }
 ];
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,44 +8,30 @@ module.exports = function (app){
 	});
 	
 	app.post("/api/friends", function(req, res) {
- 		//showing user
-    var newFriend = req.body
-   		console.log(newFriend);
-    var difference = 40;
- 		var matchName = '';
- 		var matchPhoto = '';
-       	// showing friends scores 
+ 	  var newscores = res.body.scores;
+    console.log(newscores);
+         //showing friends scores 
+    matchedScores =[];
     friendsData.forEach(function(friend){
-      matchedScores =[];
-      var totalDiff = 50;
-      console.log(friend);          
-      var Q1 = friend.q1 - newFriend.q1;
-        console.log(Q1);
-      var Q2 = friend.q2 - newFriend.q2;
-        console.log(Q2);
-      var Q3 = friend.q3 - newFriend.q3;
-        console.log(Q3);
-      var Q4 = friend.q4 - newFriend.q4;
-        console.log(Q4);     
-      var Q5 = friend.q5 - newFriend.q5;
-        console.log(Q5);
-      var Q6 = friend.q6 - newFriend.q6;
-        console.log(Q6);                                 
-      var Q7 = friend.q7 - newFriend.q7;
-        console.log(Q7);    
-      var Q8 = friend.q8 - newFriend.q8;
-        console.log(Q8);    
-      var Q9 = friend.q9 - newFriend.q9;
-        console.log(Q9);
-      var Q10 = friend.q10 - newFriend.q10;
-        console.log(Q10);
-      var Q11 = friend.q11 - newFriend.q11;
-        console.log(Q11);                    
-        var answer = Q1 + Q2 + Q3 + Q4 + Q5 + Q6 + Q7 + Q8 + Q9 + Q10 + Q11;
-        console.log("answer = " + answer);  
-    });
+      for (var i = 0; i < friend.scores.length; i++) {
+        console.log("scores = " + friend.scores[i])
+      }
+        console.log("friend =" + friend.name)
+        console.log("--------------------")
+        });
+  
 
+  });
+ };
 
- 	});	 
+  //     };           
 
-};
+  //       console.log(matchedScores);
+  //     // // pulling the lowest one 
+  //     //   var match = Math.min.apply(null, matchedScores);
+  //     //   console.log(match);
+  //    });
+  
+ 	// });	 
+
+// };
